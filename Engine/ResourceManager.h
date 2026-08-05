042af23cfab6fcfd832815bb069f641cd4863850
WilfulSquash <tmpriester.tp@gmail.com>
2026-08-05 13:07:07 -0600

    Updated ResourceManager

--- Engine/ResourceManager.h
+++ Engine/ResourceManager.h
@@ -9,41 +9,71 @@
 using namespace std;
 
 namespace nu {
-	class ResourceManager : public Singleton<ResourceManager>{
+	class ResourceManager : public Singleton<ResourceManager>
+	{
 	public:
-		template <typename T, typename ... Args>
-		res_t<T> Get(const string& name, Args&& ... args);
+		void RemoveAll() { m_resources.clear(); }
+
+		template<typename T, typename ... Args>
+			requires std::derived_from<T, Resource>
+		res_t<T> Get(const std::string& name, Args&& ... args);
+
+		template<typename T, typename ... Args>
+			requires std::derived_from<T, Resource>
+		res_t<T> GetWithID(const std::string& id, const std::string& name, Args&& ... args);
 
 	private:
-		map<string, res_t<Resource>> m_resources;
+		friend class Singleton<ResourceManager>;
+		ResourceManager() = default;
+
+	private:
+		std::map<std::string, res_t<Resource>> m_resources;
 	};
 
-	template<typename T, typename ...Args>
-	inline res_t<T> ResourceManager::Get(const string& name, Args && ...args)
+	template<typename T, typename ... Args>
+		requires std::derived_from<T, Resource>
+	inline res_t<T> ResourceManager::Get(const std::string& name, Args&& ... args)
 	{
-		auto iter = m_resources.find(name);
-		//check if resource exists
-		if (iter != m_resources.end()) {
-			auto base = iter->second; // return existing resource
-			auto resource = dynamic_pointer_cast<T>(base);
+		return GetWithID<T>(name, name, std::forward<Args>(args)...);
+	}
 
-			if (resource == nullptr) {
-				cerr << "Resource type mismatch: " << name << endl;
+	template<typename T, typename ...Args>
+		requires std::derived_from<T, Resource>
+	inline res_t<T> ResourceManager::GetWithID(const std::string& id, const std::string& name, Args && ...args)
+	{
+		auto iter = m_resources.find(id);
+		// check if exists
+		if (iter != m_resources.end())
+		{
+			// get value in iterator
+			auto base = iter->second;
+			// cast to data type T
+			auto resource = std::dynamic_pointer_cast<T>(base);
+			// check if cast was successful
+			if (resource == nullptr)
+			{
+				std::cerr << "Resource type mismatch: " << id << std::endl;
 				return res_t<T>();
 			}
+
+			// return resource
 			return resource;
 		}
-		//resource does not exist, create and load
-		res_t<T> resource = make_shared<T>();
-		if (!resource->Load(name, forward<Args>(args)...)) {
-			cerr << "Couldn't load resource: " << name << endl;
+
+		// load resource
+		res_t<T> resource = std::make_shared<T>();
+		if (resource->Load(name, std::forward<Args>(args)...) == false)
+		{
+			std::cerr << "Could not load resource: " << name << std::endl;
 			return res_t<T>();
 		}
 
-		//store resource in map
-		m_resources[name] = resource;
+		// add resource to resource manager
+		m_resources[id] = resource;
 
 		return resource;
+
 	}
-	inline ResourceManager& Resources() {return ResourceManager::Instance();}
+
+	inline ResourceManager& Resources() { return ResourceManager::Instance(); }
 }
